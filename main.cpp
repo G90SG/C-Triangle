36e841ac87358d45521d8b9666c91c89874faf82
Gemma Dingwall <gemma9261@hotmail.com>
2020-12-05 12:15:20 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// this is my first C++ code 
+#include <iostream>
+using namespace std;
+int main()
+{
+cout << "            C"<<endl;
+cout << "          i   i"<<endl;
+cout << "        s       s"<<endl;
+cout << "      b           b"<<endl;
+cout << "    e               e"<<endl;
+cout << "  s                   s"<<endl;
+cout << "t s e b s i C i s b e s t"<<endl;
+
+return 0;
+}
